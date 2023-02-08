7e30b2a5b582c61e634406696a59df4b3552d5b4
iamabigartist <pch4178@gmail.com>
2023-02-08 18:32:10 +0800

    merge forest lab

---
+++ Assets/Projects/OurForestLabs/MyUtils/ContextUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+namespace MyUtils
+{
+    public static partial class ContextUtil
+    {
+        public static StackFrame GetCurrentFrame()
+        {
+            return new StackTrace().GetFrame( 1 );
+        }
+
+        public static StackFrame GetCallerFrame()
+        {
+            return new StackTrace().GetFrame( 2 );
+        }
+
+    }
+
+}
