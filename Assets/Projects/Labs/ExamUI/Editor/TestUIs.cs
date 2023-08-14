bb517857dad862a3b0be9cf70122edac29243a81
Chua <pch4178@gmail.com>
2023-08-14 16:06:57 +0800

    finish and test tree connector

---
+++ Assets/Projects/Labs/ExamUI/Editor/TestUIs.cs
@@ -0,0 +1,52 @@
+﻿using PrototypePackages.MainThreadExecutor.Editor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static Labs.ExamUI.ExamNewUITools.TreeStructureConnect<UnityEngine.UIElements.VisualElement>;
+namespace Labs.ExamUI.Editor
+{
+public static class TestUIs
+{
+
+	[MenuItem("Labs/ExamUI/TestTreeConstruct")]
+	public static void TestTreeConstruct()
+	{
+		var window = new LiteEditorWindow("TestTreeConstruct")
+		{
+			window =
+			{
+				name = "TestTreeConstruct"
+			}
+		};
+		var root = window.root;
+		root.styleSheets.Add(Resources.Load<StyleSheet>("TestBox"));
+		TreeConnect((parent, child) => parent.Add(child),
+			(root, new Node[]
+				{
+					(new Box(), new Node[]
+						{
+							(new Label("1"), default),
+							(new Box(), new Node[]
+							{
+								(new Label("2.1"), default),
+								(new Label("2.2"), default),
+								(new Box(),new Node[]
+								{
+									(new Label("2.3.1"), default),
+									(new Label("2.3.2"), default),
+								})
+							}),
+							(new Box(), new Node[]
+							{
+								(new Label("3.1"), default),
+								(new Label("3.2"), default),
+							})
+						}
+					),
+				}
+			)
+			);
+		window.Show();
+	}
+}
+}
--- Assets/Projects/Labs/ExamUI/ExamNewUITools/TreeStructureConnect.cs
+++ Assets/Projects/Labs/ExamUI/ExamNewUITools/TreeStructureConnect.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 namespace Labs.ExamUI.ExamNewUITools
 {
-public static class TreeStructureConnect<TEnum, TNodeObject> where TEnum : Enum
+public static class TreeStructureConnect<TNodeObject>
 {
 	public struct Node
 	{
 		public TNodeObject obj;
 		public List<Node> children_list;
-		public Node(TNodeObject obj, List<Node> children_list = null)
+		public bool is_leaf_node => children_list == null;
+		public Node(TNodeObject obj, params Node[] children_list)
 		{
 			this.obj = obj;
-			this.children_list = children_list;
+			this.children_list = children_list?.ToList();
 		}
+		public static implicit operator Node((TNodeObject obj, Node[] children) p) => new(p.obj, p.children);
 	}
 	public delegate void ConnectDlgt(TNodeObject parent, TNodeObject child);
-	public static void TreeConnect(Node root, Dictionary<TEnum, ConnectDlgt> connect_methods) {}
+	public static void TreeConnect(ConnectDlgt connect_method, Node root)
+	{
+		Stack<Node> stack = new Stack<Node>();
+		stack.Push(root);
+		while (stack.TryPop(out var node))
+		{
+			if (node.is_leaf_node) { continue; }
+			foreach (var child in node.children_list)
+			{
+				connect_method(node.obj, child.obj);
+				stack.Push(child);
+			}
+		}
+	}
 }
 }
