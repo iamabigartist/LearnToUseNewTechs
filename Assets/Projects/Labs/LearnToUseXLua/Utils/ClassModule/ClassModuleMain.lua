360efde92f859b9ecc4fd3393e7bc9e6a3d7c8a4
Chua <53459343+iamabigartist@users.noreply.github.com>
2024-06-19 20:46:10 +0800

    TestClass

---
+++ Assets/Projects/Labs/LearnToUseXLua/Utils/ClassModule/ClassModuleMain.lua
@@ -0,0 +1,22 @@
+﻿local 
+
+local ClassModuleFuncList = {}
+
+local function CLassMode_index(t, k)
+    local f = ClassModuleFuncList[k]
+    if f then return f end
+    return { is_declaration = true, name = k }
+end
+
+local function CLassMode_newindex(t, k, v)
+    rawset(t, k, v)
+end
+
+ClassMode_ENV_MT = {
+    __index = function(t, k)
+        return CLassMode_index(t, k)
+    end,
+    __newindex = function(t, k, v)
+        CLassMode_newindex(t, k, v)
+    end
+}
---
+++ Assets/Projects/Labs/LearnToUseXLua/Utils/ClassModule/example.lua
@@ -0,0 +1,27 @@
+﻿Class(Cat,
+    function()
+        static_field
+        (
+            sleep_time,
+            MaxHP,
+            MaxMP,
+            Strength,
+            Agile
+        )
+
+        field
+        (
+            name,
+            meow_sound,
+            fur_color
+        )
+
+        method(Cat,
+            function(Cat, name, meow_sound, fur_color)
+                Cat.name = name
+                Cat.meow_sound = meow_sound
+                Cat.fur_color = fur_color
+            end
+        )
+    end
+)
